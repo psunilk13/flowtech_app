283fc125c015d204306429c420ac8bfedc7dbb39
psunilk13 <121981835+psunilk13@users.noreply.github.com>
2026-04-17 14:30:59 +0530

    Update oa_register_select.py

--- my_flowtech_app/flowtech/oa_register_select.py
+++ my_flowtech_app/flowtech/oa_register_select.py
@@ -1,22 +1,3 @@
-# import frappe
-
-# @frappe.whitelist()
-# def get_synced_items(doc):
-#     doc = frappe.parse_json(doc)
-
-#     updated_rows = []
-
-#     for item in enumerate(doc.get("items_details", []), start=1):
-#         updated_rows.append({
-#             "item_number": item.get("custom_serial_no"),
-#             "item_code": item.get("item"),
-#             "item_name": item.get("item_name"),
-#             "ordered_qty": item.get("quantity")
-#         })
-
-#     return updated_rows
-
-
 import frappe
 
 @frappe.whitelist()
